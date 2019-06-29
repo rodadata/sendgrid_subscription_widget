3b4bb912bf0e74a45632e36f4d8e5eb97a4e0979
xocasgv <xocasgv@gmail.com>
2019-06-29 12:33:10 +0200

    Updated to RODA

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 // Change the url to the domain of your app
-exports.url = 'http://localhost:3090';
+exports.url = 'https://roda-subscribe.herokuapp.com/';
 
-exports.senderEmail = "sender@example.com";
-exports.senderName = "Sender Name";
+exports.senderEmail = "youremail@somewhere.com";
+exports.senderName = "Name Lastname";
 
 // set 'exports.listId = null' to add contact to all contacts, but no specific list
 // or a string with the listId to add to a specific list
@@ -14,4 +14,4 @@ exports.templateId = null;
 
 // receive an email when a new signup is confirmed
 exports.sendNotification = true;
-exports.notificationEmail = "admin@example.com";
+exports.notificationEmail = "xocasgv+roda@gmail.com";
